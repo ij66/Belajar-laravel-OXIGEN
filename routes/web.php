7944c2a2c6d39a15458f4b40665a55f3aafc8400
ij66 <nigez98@gmail.com>
2022-03-19 20:15:47 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::get('/1', function () {
-    return view('home');
+    return view('welcome');
 });
